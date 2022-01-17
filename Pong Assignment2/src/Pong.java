05eac5305d5e8ab2da779fa32bc5d8ef8ac65f54
Jen44 <jenny.olb@hotmail.com>
2022-01-18 00:50:09 +1300

    Adding completed Pong game

---
+++ Pong Assignment2/src/Pong.java
@@ -0,0 +1,29 @@
+import javax.swing.JFrame;
+
+public class Pong extends JFrame {
+
+	static final int SCREEN_WIDTH = 800;
+	static final int SCREEN_HEIGHT = 600;
+	static final String WINDOW_TITLE = "Pong";
+
+	public Pong() {
+		setTitle(WINDOW_TITLE);
+		setSize(SCREEN_WIDTH, SCREEN_HEIGHT);
+		setResizable(false);
+		add(new PongPanel());
+		setVisible(true);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+	}
+
+	public static void main(String[] args) {
+		javax.swing.SwingUtilities.invokeLater(new Runnable() {
+
+			public void run() {
+
+				new Pong();
+			}
+
+		});
+	}
+
+}
